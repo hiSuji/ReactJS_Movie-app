2ee9644e824e7c0be9fdc390f45336f3ccfba9e7
Suji kim <sujikim.dev@gmail.com>
2019-09-24 16:03:49 +0900

    Implement the movie props

--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis';
+import './Movie.css';
 
 function Movie(props) {
     return (
-        <div>
-            <MoviePoster poster={props.poster}/>
-            <h1 >
-                {props.title}
-            </h1>
+        <div className="Movie">
+            <div className="Columns">
+                <MoviePoster poster={props.poster} alt={props.title}/>
+            </div>
+            <div className="Columns">
+                <h1>{props.title}</h1>
+                <div className="Genres">
+                    {props.genres.map((genre, index) => <MovieGenre genre = {genre} key={index} />)}
+                </div>
+                <div className="Synopsis">
+                    <LinesEllipsis
+                        text={props.synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'/>
+                </div>
+            </div>
         </div>
     );
 }
 
-Movie.propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+function MoviePoster(props) {
+    return (<img src={props.poster} alt={props.alt} title={props.alt} className="Poster"/>);
 }
 
-function MoviePoster(props) {
-    return (<img src={props.poster} alt="movie poster"/>);
+function MovieGenre(props) {
+    return (<span className="Genre">{props.genre}</span>);
 }
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
 
 export default Movie;
